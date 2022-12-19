ec4278e3d18e182926b2d5d376d847904828ce15
pengabutiken <theonils9@gmail.com>
2022-12-19 08:00:13 +0000

    add structure to js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,24 @@
-console.log('connected')
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
